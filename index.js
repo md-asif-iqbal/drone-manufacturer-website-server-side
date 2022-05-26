3324b51e4dd8a7178601aa9232a2c518235c8c8b
Asif Iqbal <asifiqbal@Asifs-MBP.Dlink>
2022-05-26 18:28:55 +0600

    admin done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-
 require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -30,6 +30,19 @@ function verifyJWT(req, res, next) {
   }
 
 
+  // use Admin Function
+  const verifyAdmin = async (req, res, next) => {
+    const requester = req.decoded.email;
+    const requesterAccount = await userCollection.findOne({ email: requester });
+    if (requesterAccount.role === 'admin') {
+      next();
+    }
+    else {
+      res.status(403).send({ message: 'forbidden' });
+    }
+  }
+
+
   async function run(){
       try{
         await client.connect();
@@ -37,7 +50,7 @@ function verifyJWT(req, res, next) {
         const purchaseCollection = client.db('drone_kits').collection('purchase');
         const userCollection = client.db('drone_kits').collection('users');
         const reviewCollection = client.db('drone_kits').collection('reviews');
-        const profileCollection = client.db('drone_kits').collection('profile');
+        const paymentCollection = client.db('drone_kits').collection('payment');
 
 
         // User Access Token
@@ -53,6 +66,7 @@ function verifyJWT(req, res, next) {
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10h' })
             res.send({ result, token });
           });
+      
         // Get Parts here 
         app.get('/parts' ,  async(req , res) =>{
           const quary ={}
@@ -80,6 +94,20 @@ function verifyJWT(req, res, next) {
       const result = await purchaseCollection.insertOne(purchase);
       return res.send({ success: true, result });
     });
+    // payment pruchase update......
+    app.patch('/purchase/:id', verifyJWT, async(req, res) =>{
+      const id  = req.params.id;
+      const payment = req.body;
+      const filter = {_id: ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId
+        }}
+      const result = await paymentCollection.insertOne(payment);
+      const updatedPurchase = await purchaseCollection.updateOne(filter, updatedDoc);
+      res.send(updatedPurchase);
+    })
 
     // find My Order
     app.get('/purchase' , verifyJWT, async(req , res) =>{
@@ -134,7 +162,13 @@ function verifyJWT(req, res, next) {
     });
     // <<<<<------Get Review Here------>>>>>>
 
-
+    app.get('/reviews' ,  async(req , res) =>{
+      const quary ={}
+      const cursor = reviewCollection.find(quary);
+      const result = await cursor.toArray();
+      res.send(result)
+  
+    })
   // Update user..........
 
     app.put('/users/:email', async (req, res) => {
@@ -167,11 +201,51 @@ function verifyJWT(req, res, next) {
         const purchased = await userCollection.find(quary).toArray();
         res.send(purchased);
       }
+     
+
       else{
         return res.status(403).send({ message: 'Forbidden Access'})
       }
       
     })
+     // _______>>>>>>> Users alll--------<<<<<>>>>>
+    app.get('/user', verifyJWT, verifyAdmin, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+    // <<<<<<Payments here >>>>>>>>
+    app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
+      const service = req.body;
+      console.log(service);
+      const price = service.totalPrice;
+      const amount = price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount : amount,
+        currency: 'usd',
+        payment_method_types:['card']
+      });
+      res.send({clientSecret: paymentIntent.client_secret})
+    });
+    // ========here--------
+    // admin here========
+    app.get('/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === 'admin';
+      res.send({ admin: isAdmin })
+    })
+    // -----user admin email find-------
+    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: 'admin' },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
 
       }
        finally{
